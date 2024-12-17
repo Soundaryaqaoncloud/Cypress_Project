6c7264bc0dc00657c09c40e3a8a3b8e388e12af5
Soundaryaqaoncloud <soundarya@qaoncloud.com>
2024-12-17 18:43:39 +0530

    Delete cypress/e2e/1-getting-started directory

--- cypress/e2e/1-getting-started/Draganddrop.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Drag and Drop Test Suite', () => {
-
-    it('Drag event to calender', () => {
-        cy.visit('https://fullcalendar.io/docs/external-dragging-demo')
-        cy.get(':nth-child(2) > .fc-event-main').drag(':nth-child(1) > .fc-day-wed > .fc-daygrid-day-frame')
-    
-    })
-
-})
