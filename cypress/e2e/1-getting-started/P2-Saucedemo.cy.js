f5f710c6f9c6df10ff11313766146607b97aa908
Soundaryaqaoncloud <soundarya@qaoncloud.com>
2025-01-25 12:17:04 +0530

    Add files via upload

---
+++ cypress/e2e/1-getting-started/P2-Saucedemo.cy.js
@@ -0,0 +1,28 @@
+
+describe('Practical 2 - Saucedemo', () => {
+    it('Saucedemo login, search a product, product listing, Add to cart and amount calculation)', () => { 
+    cy.visit("https://www.saucedemo.com/")
+    cy.wait(1000)
+    cy.get("#user-name").type("standard_user")
+    cy.get("#password").type("secret_sauce")
+    cy.get("#login-button").click()
+    cy.get('.product_sort_container').select('az')
+    cy.get('.product_sort_container').select('za')
+    cy.get('.product_sort_container').select('lohi')
+    cy.get('.product_sort_container').select('hilo')
+    cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click()
+    cy.get('.shopping_cart_link').click()
+    cy.get('[data-test="item-quantity"]').should('have.length', '1')
+    cy.get('[data-test="checkout"]').click()
+    cy.get('[data-test-"firstName"]').type("Soundarya")
+    cy.get('[data-test="lastName"]').type("5")
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test-"postalCode"]').type("607308")
+    cy.get('[data-test="subtotal-label"]').should('have.text','Item total: $49.99')
+    cy.get('[data-test="tax-label"]').should('have.text', 'Tax: $4.00')
+    cy.get('[data-test="total-label"]').should('have.text', 'Total: $53.99')
+    cy.get('[data-test="finish"]').click()
+    cy.get("[data-test=”complete-header]").should('have.text',"Thank you for your order!")
+    
+    })
+})
